d5f7ba985bf9c7785b5b35af866f02a9bc493e27
eleibowi <eleibowi@andrew.cmu.edu>
2015-12-01 09:22:18 +1030

    Moved GUI to controller

--- src/bookstore/Controller.java
+++ src/bookstore/Controller.java
@@ -1,12 +1,18 @@
 package bookstore;
 
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
+
+import javax.swing.*;
 
 public class Controller {
 	private Inventory inventory;
 	private ArrayList<Transaction> transactions;
 	private ArrayList<OrderRequest> orderRequests;
 	private ShoppingCart shoppingCart;
+	private JFrame frame;
 	
 	public Controller(){
 		inventory = new Inventory();
@@ -15,6 +21,91 @@ public class Controller {
 		inventory.addBook(4, "Gone with the Wind", "Margaret Mitchell", 1, 5, 24, 0, 0);
 		inventory.displayInventory();
 		shoppingCart=new ShoppingCart();
+		
+		frame = new JFrame("Rinku's Bookstore");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(1300,700);
+		//frame.setResizable(false);
+		
+		JPanel checkout = new JPanel();
+		checkout.setLayout(new GridLayout(1,2));
+		
+		JPanel currentBooks = new JPanel();
+		currentBooks.setLayout(new GridLayout(shoppingCart.getBooks().size()+1,5));
+		currentBooks.add(new JLabel("Title"));
+		currentBooks.add(new JLabel("Author"));
+		currentBooks.add(new JLabel("Edition"));
+		currentBooks.add(new JLabel("ISBN"));
+		currentBooks.add(new JLabel(""));
+		//findBooks.add(new JLabel("Used Price"));
+		//findBooks.add(new JLabel("New Price"));
+		for(BookPurchase b:shoppingCart.getBooks()){
+			currentBooks.add(new JLabel(b.getTitle()));
+			currentBooks.add(new JLabel(b.getAuthor()));
+			currentBooks.add(new JLabel(""+b.getEdition()));
+			currentBooks.add(new JLabel(""+b.getIsbn()));
+			JButton addToCart = new JButton("Remove from cart");
+			addToCart.putClientProperty("book", b);
+			addToCart.addActionListener(new ActionListener(){
+				public void actionPerformed(ActionEvent event){
+					shoppingCart.removeBook((BookPurchase)((JButton)event.getSource()).getClientProperty("book"));
+					
+				}
+			});
+			currentBooks.add(addToCart);
+			//findBooks.add(new JLabel(""+b.getUsedPrice()));
+			//findBooks.add(new JLabel(""+b.getNewPrice()));
+		}
+		
+		JPanel findBooks = new JPanel();
+		findBooks.setLayout(new GridLayout(inventory.getBooks().size()+1,6));
+		findBooks.add(new JLabel("Title"));
+		findBooks.add(new JLabel("Author"));
+		findBooks.add(new JLabel("Edition"));
+		findBooks.add(new JLabel("ISBN"));
+		findBooks.add(new JLabel("Quality"));
+		findBooks.add(new JLabel(""));
+		//findBooks.add(new JLabel("Used Price"));
+		//findBooks.add(new JLabel("New Price"));
+		for(BookEntry b:inventory.getBooks()){
+			findBooks.add(new JLabel(b.getTitle()));
+			findBooks.add(new JLabel(b.getAuthor()));
+			findBooks.add(new JLabel(""+b.getEdition()));
+			findBooks.add(new JLabel(""+b.getIsbn()));
+			String[] newUsedStrings = {"New: $"+b.getNewPrice(),"Used: $"+b.getUsedPrice()};
+			JComboBox<String> newUsed = new JComboBox<String>(newUsedStrings);
+			findBooks.add(newUsed);
+			JButton addToCart = new JButton("Add to cart");
+			addToCart.putClientProperty("book", b);
+			addToCart.addActionListener(new ActionListener(){
+				public void actionPerformed(ActionEvent event){
+					shoppingCart.addBook((BookEntry)((JButton)event.getSource()).getClientProperty("book"), 1, false);
+					System.out.println(shoppingCart.getBooks());
+					
+				}
+			});
+			findBooks.add(addToCart);
+			//findBooks.add(new JLabel(""+b.getUsedPrice()));
+			//findBooks.add(new JLabel(""+b.getNewPrice()));
+		}
+		
+		checkout.add(currentBooks);
+		checkout.add(findBooks);
+		
+		JPanel returnPurchase = new JPanel();
+		
+		JPanel buyFromCustomer = new JPanel();
+		
+		JPanel manageInventory = new JPanel();
+		
+		JTabbedPane tabbedPane=new JTabbedPane();
+		tabbedPane.addTab("Checkout", checkout);
+		tabbedPane.addTab("Return Purchase", returnPurchase);
+		tabbedPane.addTab("Buy From Customer", buyFromCustomer);
+		tabbedPane.addTab("Manage Inventory", manageInventory);
+		frame.add(tabbedPane);
+		
+		frame.setVisible(true);
 	}
 	
 	public void checkOut(){
